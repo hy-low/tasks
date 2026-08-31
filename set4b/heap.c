560bf045cfc08f84b26f33e84d3db8f52840c68a
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2026-08-31 11:09:58 +0300

    Add set 4b

---
+++ set4b/heap.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "heap.h"
+
+struct Heap {
+    // TODO
+};
+
+Heap* heap_create(void) {
+    // TODO
+}
+
+void heap_push(Heap *heap, int value) {
+    // TODO
+}
+
+void heap_pop(Heap *heap) {
+    // TODO
+}
+
+int heap_top(Heap *heap) {
+    // TODO
+}
+
+int heap_size(Heap *heap) {
+    // TODO
+}
+
+void heap_free(Heap *heap) {
+    // TODO
+}
