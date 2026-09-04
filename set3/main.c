7bbe05e2590f8a9de12d5a80af37d3ffe34f2417
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2026-09-04 16:52:33 +0300

    Change test

--- set3/main.c
+++ set3/main.c
@@ -185,7 +185,6 @@ int main(void) {
     test_string(trim(" aybabtu   "), "aybabtu");
     test_string(trim("aybabtu"), "aybabtu");
     test_string(trim("    aybabtu    "), "aybabtu");
-    test_string(trim("    ayb abtu    "), "ayb abtu");
     test_string(trim("   "), "");
 
     test_string(upper("aybabtu"), "AYBABTU");
